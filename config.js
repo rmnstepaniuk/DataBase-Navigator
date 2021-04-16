a2d0f8508a83be58fd1ed76e04305c40055e70fd
rmnstepaniuk <rmnstepaniuk@users.noreply.github.com>
2021-04-16 16:26:08 +0300

    version 0.4.0
    Addition of ejs

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const PORT = process.env.JAWSDB_URL || 3000;
+const PORT = process.env.JAWSDB_URL || 8000;
 const srcDir = path.resolve(__dirname, 'src');
 
 module.exports = {PORT : PORT, srcDir : srcDir};
--- index.js
+++ index.js
@@ -2,39 +2,65 @@ const express = require('express');
 const config = require('./config');
 const mysql = require('mysql2');
 
-
 const app = express();
 
-app.use(express.static(config.srcDir));
-// const connectionURL = process.env.JAWSBD_URL; //|| 'mysql://user:password@host:name/default_schema'
+app.set('view engine', 'ejs');
+app.set('views', 'src');
+
+// function addDatabase(host, user, password, database) {
+//     var sqlDatabase = mysql.createConnection({
+//         host: host,
+//         user: user,
+//         password: password,
+//         database: database
+//     });
+//     sqlDatabase.connect((error) => {
+//         if (error) console.log(error);
+//         console.log('Connected');
+//         databases.push(database)
+//     });
+//     app.get('/', (req, res) => {
+//         res.render('index', {databases})
+//     });
+// }
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on PORT ${config.PORT}...`);
 });
 
-var database = mysql.createConnection({
+var databases = []
+
+var database = {
     host: 'd3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
     user: 'm2725ei61timi8ut',
     password: 'vcae6egn6tnmmaio',
     database: 'b2v6pffi6qrk4ubk'
-});
+}
 
-database.connect(function(error) {
+var sqlDatabase = mysql.createConnection(database);
+sqlDatabase.connect((error) => {
     if (error) console.log(error);
     console.log('Connected');
+    databases.push(database)
 });
 
-function queryRequest(event) {
-    event.preventDefault();
-    let entry = document.getElementById("dbquery");
-    console.log(entry.value);
-};
-
-app.get('/db', function(req, res) {
-    let sql = 'SELECT user.name, access.role FROM access, user WHERE access.user_id = user.id;';
-    database.query(sql, function(error, result) {
-        if (error) console.log(error);
-        console.log(result);
-        res.send(result);
-    });
+app.get('/', (req, res) => {
+    let sql = req.query.sql;
+    if (sql) {
+        sqlDatabase.query(sql, (error, result, fields) => {
+            if (error) {
+                console.log(error);
+            }
+            result = result.map(row => 
+                fields.map(f => ({
+                    name : f.name,
+                    value : row[f.name]
+                }))
+                )
+                console.log(result);
+                res.render('index', {databases, result});
+        });
+    } else {
+        res.render('index', {databases, result : {}})
+    }
 });
